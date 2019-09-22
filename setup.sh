2b3877276df8430d2f673c69ee9fcd7fbb5bcdbc
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-09-22 11:05:58 +0100

    First iteration of config with dirty script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cp configs/Xresources $HOME/.
+xrdb $HOME/Xresources
+
+cp $HOME/.config/i3/config $HOME/.config/i3/config.bak
+cp configs/i3.conf $HOME/.config/i3/config
+
+cp configs/tint2.conf $HOME/.config/tint2/.
