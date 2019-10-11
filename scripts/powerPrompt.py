8a742412d4a42229f21bfe4e957d13e6c6c1a942
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-10-11 19:20:13 +0100

    Add cairo for drawing

--- scripts/powerPrompt.py
+++ scripts/powerPrompt.py
@@ -1,8 +1,27 @@
 import os
 
+import cairo
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+
+def draw(da, ctx):
+
+    ctx.set_source_rgb(0,0,0)
+    ctx.set_line_width(20 / 4)
+    #ctx.set_tolerance(0.1)
+
+    # Drawing shapes
+    ctx.save()
+    ctx.new_path()
+
+    ctx.rectangle(0,0,40,40)
+
+    ctx.close_path()
+
+    ctx.stroke()
+
+    ctx.restore()
 
 class ButtonWindow(Gtk.Window):
 
@@ -24,6 +43,10 @@ class ButtonWindow(Gtk.Window):
         button = Gtk.Button.new_with_mnemonic("_Close")
         button.connect("clicked", self.on_close_clicked)
         hbox.pack_start(button, True, True, 0)
+
+        drawingArea = Gtk.DrawingArea()
+        hbox.pack_start(drawingArea, True, True, 0)
+        drawingArea.connect('draw', draw)
 
     def on_shutdown_clicked(self, button):
         os.system("systemctl poweroff")
