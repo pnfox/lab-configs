b6826f7c3fc9b8b8942957f23d24a9f4b9f9b4a0
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-10-11 20:01:56 +0100

    Add icons

--- scripts/powerPrompt.py
+++ scripts/powerPrompt.py
@@ -9,13 +9,13 @@ def draw(da, ctx):
 
     ctx.set_source_rgb(0,0,0)
     ctx.set_line_width(20 / 4)
-    #ctx.set_tolerance(0.1)
 
     # Drawing shapes
     ctx.save()
     ctx.new_path()
 
-    ctx.rectangle(0,0,40,40)
+    ctx.arc(300, 300, 40, 0, 2*3.14159)
+    ctx.rectangle(300,300,0,40)
 
     ctx.close_path()
 
@@ -32,21 +32,32 @@ class ButtonWindow(Gtk.Window):
         hbox = Gtk.Box(spacing=6)
         self.add(hbox)
 
-        button = Gtk.Button.new_with_label("Shutdown")
-        button.connect("clicked", self.on_shutdown_clicked)
-        hbox.pack_start(button, True, True, 0)
+        #button = Gtk.Button.new_with_label("Shutdown")
+        #button.connect("clicked", self.on_shutdown_clicked)
+        #hbox.pack_start(button, True, True, 0)
 
-        button = Gtk.Button.new_with_label("Reboot")
-        button.connect("clicked", self.on_reboot_clicked)
-        hbox.pack_start(button, True, True, 0)
+        #button = Gtk.Button.new_with_label("Reboot")
+        #button.connect("clicked", self.on_reboot_clicked)
+        #hbox.pack_start(button, True, True, 0)
 
-        button = Gtk.Button.new_with_mnemonic("_Close")
-        button.connect("clicked", self.on_close_clicked)
-        hbox.pack_start(button, True, True, 0)
+        #button = Gtk.Button.new_with_mnemonic("_Close")
+        #button.connect("clicked", self.on_close_clicked)
+        #hbox.pack_start(button, True, True, 0)
 
+        powerIcon = Gtk.Image()
+        powerIcon.set_from_file("./powerOff.png")
+        hbox.pack_start(powerIcon, True, True, 0)
+
+        powerIcon = Gtk.Image()
+        powerIcon.set_from_file("./reboot.png")
+        hbox.pack_start(powerIcon, True, True, 0)
+        powerIcon = Gtk.Image()
+        powerIcon.set_from_file("./suspend.png")
+        hbox.pack_start(powerIcon, True, True, 0)
+        
         drawingArea = Gtk.DrawingArea()
-        hbox.pack_start(drawingArea, True, True, 0)
         drawingArea.connect('draw', draw)
+        drawingArea.connect("button-press-event", self.on_close_clicked)
 
     def on_shutdown_clicked(self, button):
         os.system("systemctl poweroff")
