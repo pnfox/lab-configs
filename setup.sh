5df8135b1645e31fbe258fffd74bd3dc9e8e26fa
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-10-24 19:12:05 +0100

    Place power icons in /usr/share/icons

--- setup.sh
+++ setup.sh
@@ -48,6 +48,10 @@ then
 
     cp configs/launchy.png /usr/share/icons/.
 
+    cp background/powerOff.png /usr/share/icons/.
+    cp background/reboot.png /usr/share/icons/.
+    cp background/suspend.png /usr/share/icons/.
+
     cp background/default.png /usr/share/backgrounds/default.png
 
 fi
@@ -76,6 +80,10 @@ then
 
     mkdir $HOME/.config/icons/
     cp configs/launchy.png $HOME/.config/icons/.
+
+    cp background/powerOff.png /usr/share/icons/.
+    cp background/reboot.png /usr/share/icons/.
+    cp background/suspend.png /usr/share/icons/.
 
     cp background/default.png /usr/share/backgrounds/default.png
 
